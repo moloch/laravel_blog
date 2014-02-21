4b18820f8e67e026cd0bcf2d6ed88ae57ffbd1cb
dario <dario.coco@gmail.com>
2014-02-21 19:52:59 +0100

    more assertions in LoginTest

--- app/tests/LoginTest.php
+++ app/tests/LoginTest.php
@@ -6,6 +6,10 @@ class LoginTest extends TestCase {
 		$this -> createTestUser();
 		$response = $this -> login('dario.coco@gmail.com','ciaociao');
 		$this -> assertEquals($response -> getContent(), 'Login successful');
+		$response = $this -> login('dario.coco@gmail.com','ciaociao123');
+		$this -> assertEquals($response -> getContent(), 'Invalid username or password');
+		$response = $this -> login('dario.coco666@gmail.com','ciaociao123');
+		$this -> assertEquals($response -> getContent(), 'Invalid username or password');
 	}
 	
 	private function login($email, $password){
