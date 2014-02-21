a979996f9105adbe63e91961d8cd03384796306e
dario <dario.coco@gmail.com>
2014-02-21 19:49:26 +0100

    logintest refactoring

--- app/tests/LoginTest.php
+++ app/tests/LoginTest.php
@@ -4,10 +4,15 @@ class LoginTest extends TestCase {
 
 	public function testPostLogin() {
 		$this -> createTestUser();
-		$parameters = array('email' => 'dario.coco@gmail.com', 'password' => 'ciaociao');
+		$response = $this -> login('dario.coco@gmail.com','ciaociao');
+		$this -> assertEquals($response -> getContent(), 'Login successful');
+	}
+	
+	private function login($email, $password){
+		$parameters = array('email' => $email, 'password' => $password);
 		$crawler = $this -> client -> request('POST', 'login', $server = $parameters);
 		$response = $this -> client -> getResponse();
-		$this -> assertEquals($response -> getContent(), 'Login successful');
+		return $response;
 	}
 
 }
