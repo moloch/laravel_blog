78452721818cef0c5ff5b3668978403fdead25c0
dario <dario.coco@gmail.com>
2014-02-25 18:15:38 +0100

    comments visualization in post template

--- app/views/post.blade.php
+++ app/views/post.blade.php
@@ -6,6 +6,12 @@
     <h3>{{ $post->title }}</h3>
     <p> {{ $post->body }}</p>
     <p> Posted by: {{ $post->user->email }}</p>
+    
+    <h3>Comments</h3>
+    @foreach ($post->comments as $comment)
+        <p>{{ $post->user->email }}:</p>
+    	<p>{{ $comment->body }}</p>
+    @endforeach
 
     @if ($email === $post->user->email)
     	{{ Form::open(array('url' => 'delete/'.$post->id)) }}
