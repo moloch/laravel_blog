74a8162589f4d7f18e4b8d32301b1ca9e5dea4d8
dario <dario.coco@gmail.com>
2014-02-25 17:08:04 +0100

    Removed unused methods in Postcontroller

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -18,26 +18,4 @@ class PostController extends BaseController {
 		return View::make('post',$params);
 	}
 
-	public function showNewPostForm($id) {
-		return View::make('post');
-	}
-
-	public function addPost() {
-		$new_post = array('title' => Input::get('title'),
-		 'body' => Input::get('body'),
-		 'user_id' => Input::get('user_id'));
-
-		$rules = array('title' => 'required|min:3|max:64', 'body' => 'required|min:10');
-
-		$validation = Validator::make($new_post, $rules);
-		if ($validation -> fails()) {
-			return Response::json("KO");
-		}
-		// create the new post after passing validation
-		$post = new Post($new_post);
-		$post -> save();
-		// redirect to viewing all posts
-		return Response::json("array('post_id' => ".$post->id.")");
-	}
-
 }
